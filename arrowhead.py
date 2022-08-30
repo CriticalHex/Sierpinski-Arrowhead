b75e3776aa364cb102c1c28ed604471db4deab5a
Terminal Tyrant <kubaska05@gmail.com>
2022-08-30 14:47:59 -0600

    needs recursion

--- arrowhead.py
+++ arrowhead.py
@@ -3,8 +3,58 @@ from math import sin, cos, sqrt, pi
 
 
 class Trapezoid:
-    def __init__(self):
-        pass
+    def __init__(self,x,y,scale,trapID):
+        self.ID = trapID
+        self.scale = scale
+        if trapID == 1:
+            self.p0x = x
+            self.p0y = y
+            self.p1x = scale * cos((5*pi)/3) + self.p0x
+            self.p1y = scale * sin((5*pi)/3) + self.p0y
+            self.p2x = self.p1x + scale
+            self.p2y = self.p1y
+            self.p3x = scale * cos((5*pi)/3) + self.p2x
+            self.p3y = scale * sin((pi)/3) + self.p2y
+        if trapID == 2:
+            self.p0x = x
+            self.p0y = y
+            self.p1x = scale * cos((pi)/3) + self.p0x
+            self.p1y = scale * sin((pi)/3) + self.p0y
+            self.p2x = scale * cos((2*pi)/3) + self.p1x
+            self.p2y = scale * sin((2*pi)/3) + self.p1y
+            self.p3x = scale * cos(pi) + self.p2x
+            self.p3y = scale * sin(pi) + self.p2y
+        if trapID == 3:
+            self.p0x = x
+            self.p0y = y
+            self.p1x = scale * cos((2*pi)/3) + self.p0x
+            self.p1y = scale * sin((2*pi)/3) + self.p0y
+            self.p2x = scale * cos((pi)/3) + self.p1x
+            self.p2y = scale * sin((pi)/3) + self.p1y
+            self.p3x = scale + self.p2x
+            self.p3y = self.p2y
+        self.points = [(self.p0x,self.p0y),(self.p1x,self.p1y),(self.p2x,self.p2y),(self.p3x,self.p3y)]
+        self.color = (0,255,0)
+            
+    
+    def draw(self):
+        pygame.draw.aalines(screen,self.color,False,self.points)
+
+    def expand(self):
+        global traps2
+        traps2.clear()
+        if self.ID == 1:
+            print("ayo")
+            traps2.append(Trapezoid(self.p1x,self.p1y,self.scale/2,2))
+            traps2.append(Trapezoid(self.p1x,self.p1y,self.scale/2,1))
+            traps2.append(Trapezoid(self.p2x,self.p2y,self.scale/2,3))
+            return
+        if self.ID == 2:
+            pass
+        if self.ID == 3:
+            pass
+
+        
 
 
 pygame.init()
@@ -13,6 +63,15 @@ pygame.display.set_caption("ArrowHead")
 screen = pygame.display.set_mode((1920, 1080))
 screen.fill((0,0,0))
 Running = True
+
+# T1 = Trapezoid(500,500,200,1)
+# T2 = Trapezoid(T1.p0x,T1.p0y,200,2)
+# T3 = Trapezoid(T1.p3x,T1.p3y,200,3)
+
+traps = [Trapezoid(600,800,400,1)]
+traps2 = []
+
+test = True
 
 while Running:
     for event in pygame.event.get():
@@ -23,14 +82,19 @@ while Running:
     if keys[pygame.K_LCTRL]:
         Running = False
 
-    screen.fill((0,0,0))
     
-    startx = 500
-    starty = 500
-    line1 = (200 * cos((5*pi)/3) + startx, 200 * sin((5*pi)/3) + starty)
-    line2 = (line1[0] + 200,line1[1])
-    line3 = (200 * cos((5*pi)/3) + line2[0],200 * sin((pi)/3) + line2[1])
-    print(line1,line2,line3)
-    pygame.draw.aalines(screen, (0,255,0), False, [(startx,starty),line1,line2,line3])
+    
+    if test:
+        
+        
+        for trap in traps:
+            trap.draw()
+            trap.expand()
+        traps = traps2.copy()
+        test = False
+        screen.fill((0,0,0))
+        for trap in traps:
+            trap.draw()
+            trap.expand()
 
-    pygame.display.flip()
+        pygame.display.flip()
